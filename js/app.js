fb6d8938c2fb4ae23ed6600f19029f3a6236ed84
moi282320ne-arch <moi282320ne@gmail.com>
2026-07-18 22:18:51 -0300

    Criada estrutura JavaScript

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let ganhos = 0;
+let gastos = 0;
+let corridas = 0;
+let km = 0;
+
+function iniciarExpediente() {
+    alert("Expediente iniciado!");
+}
+
+function encerrarExpediente() {
+    alert("Expediente encerrado!");
+}
+
+function atualizarDashboard() {
+    console.log("Dashboard atualizado");
+}
