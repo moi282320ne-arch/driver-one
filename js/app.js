6da542f660e6782ba15330e4b73d3772f8c5e8a7
moi282320ne-arch <moi282320ne@gmail.com>
2026-07-18 22:25:11 -0300

    Adicionado sistema de armazenamento local

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
+// Dados do dia
 let ganhos = 0;
-let gastos = 0;
 let corridas = 0;
 let km = 0;
+let combustivel = 0;
 
-function iniciarExpediente() {
-    alert("Expediente iniciado!");
+// Registrar corrida
+function registrarCorrida(valor, distancia) {
+    ganhos += Number(valor);
+    corridas++;
+    km += Number(distancia);
+
+    salvarDados();
 }
 
-function encerrarExpediente() {
-    alert("Expediente encerrado!");
+// Salvar dados
+function salvarDados() {
+    localStorage.setItem("ganhos", ganhos);
+    localStorage.setItem("corridas", corridas);
+    localStorage.setItem("km", km);
 }
 
-function atualizarDashboard() {
-    console.log("Dashboard atualizado");
+// Carregar dados
+function carregarDados() {
+    ganhos = Number(localStorage.getItem("ganhos")) || 0;
+    corridas = Number(localStorage.getItem("corridas")) || 0;
+    km = Number(localStorage.getItem("km")) || 0;
 }
+
+carregarDados();
